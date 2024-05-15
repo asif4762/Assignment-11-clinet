6d84d2ed805166d07df376907cf5bed889061f8b
asif4762 <zasif855@gmail.com>
2024-05-15 14:22:44 +0600

    some chnages done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,8 +4,24 @@ import { AuthContext } from '../../Firebase/Provider/AuthProvider';
 import Swal from 'sweetalert2';
 
 const Navbar = () => {
+    const [theme, setTheme] = useState('light');
     const [data, setData] = useState([]);
     const { user, logOut } = useContext(AuthContext);
+
+    useEffect(() => {
+        localStorage.setItem('theme', theme);
+        const localTheme = localStorage.getItem('theme');
+        document.querySelector('html').setAttribute('data-theme', localTheme);
+    }, [theme])
+
+    const handleToggle = (e) =>{
+        if(e.target.checked){
+            setTheme("dark");
+        }
+        else{
+            setTheme("light");
+        }
+    }
 
     const url = `http://localhost:5500/my-assignments?email=${user?.email}`;
 
@@ -70,7 +86,7 @@ const Navbar = () => {
                 <div className="navbar-end">
                 <label className="flex cursor-pointer gap-2">
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
-  <input type="checkbox" value="dark" className="toggle theme-controller"/>
+  <input onChange={handleToggle} type="checkbox" value="dark" className="toggle theme-controller"/>
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
 </label>
                     {
